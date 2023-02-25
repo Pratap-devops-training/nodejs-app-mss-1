574112abaa90e9fe3ee60b316e1110b7ca388ed0
Pratap <105116480+Pratap2222@users.noreply.github.com>
2023-02-25 15:48:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Welcome to web page</h1>');
+  res.end('<h1>Welcome to nodejs Web page</h1>');
 });
 
 server.listen(9981, () => {
