ff1581b34a5be117eb7744fe2fb19d7b876fd831
Pratap <105116480+Pratap2222@users.noreply.github.com>
2023-02-25 15:54:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,5 @@ server.listen(9981, () => {
   console.log('Server running on port 9981');
 });
 
-const express = require('express');
-const app = express();
 
-app.get('/pratap', (req, res) => {
-  res.send('<h1>Hi Pratap</h1>');
-});
-
-app.listen(9981, () => {
-  console.log('Server running on port 9981');
-});
 
