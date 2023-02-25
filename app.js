d4e7c2c2edf4a66d8bb74104f017b109ab64223c
Pratap <105116480+Pratap2222@users.noreply.github.com>
2023-02-25 15:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ res.send("<h2>Welcome to Node JS express application</h2>"+appEnv.url+appEnv.por
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
-    //response.send("<h2><center>Welcome to Node JS app</h2>");
+app.get('/pratap', function(request, response) {
+    //response.send("<h2><center>Hi Welcome to Test Web page</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to Pratap test page<center></h2>" );
     response.end();
     
   })
@@ -39,18 +39,7 @@ app.get("/html", function(req,res){
     res.end();
     
     });
-    app.get("/jsonData", function(req,res){
-        res.type('json');
-        //res.type('application/json');
-        //res.json({'name': 'Mithun Reddy L'});
-        res.send({
-		'name': 'Mithun Technologies',
-		'technology': 'DevOps',
-		'contact' : '9980923226',
-		'email': 'devopstrainingblr@gmail.com'
-	            });
-        
-        });
+    
 app.get("/queryparam", function(req,res){
 //res.send(req.query);
 res.send(req.query.key + ": " + req.query.name);
@@ -68,10 +57,7 @@ app.get("/status-code-500", function(req, res) {
    res.status(500).send('Internal Server Error – custom message');
 })
 
-app.get('/redirect', function(req, res) {
-    //Send status 300
-        res.redirect('http://mithuntechnologies.com');
-    });
+
     
  
     app.listen(app.get('port'), function() {
